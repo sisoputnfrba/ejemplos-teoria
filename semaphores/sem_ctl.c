8920b9dd3603b1412d4ea3c67c8ef182961180d6
Esteban Masoero <esigma5@gmail.com>
2020-05-03 19:27:15 -0300

    Adding semaphores example

---
+++ semaphores/sem_ctl.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int s_open(sem_t** semaphore, char * sem_name);
+
+int main(int argc, char** argv) {
+	
+	char *sem_name = argv[1];
+	char *operation = argv[2];
+
+	if (argc != 2){
+		printf("Uso incorrecto\n\tEjemplo: ./a.out un_semaforo get_value|wait|post\n");
+		return EXIT_FAILURE;
+	}
+
+	sem_t* semaphore;
+	int failed = s_open(&semaphore, sem_name);
+
+	if (failed) {
+		puts("exiting....");
+		return EXIT_FAILURE;
+	}
+
+	if (!strcmp(operation,"wait")){
+		puts("- Doing wait...");
+		sem_wait(semaphore);
+		puts("- Wait done");
+	}
+	if (!strcmp(operation,"post")){
+		puts("- Doing post...");
+		sem_post(semaphore);
+		puts("- Post done");
+	}
+
+	if (!strcmp(operation,"get_value")){
+		puts("- Getting current value...");
+		int current_value;
+		sem_getvalue(semaphore, &current_value);
+		printf("- Current value: %d\n", current_value);
+	}
+
+	return EXIT_SUCCESS;
+}
+
+int s_open(sem_t** semaphore, char * sem_name){
+	//printf("Opening semaphore: %s\n", sem_name);
+	sem_t * openned_semaphore = sem_open(sem_name, O_CREAT, S_IRWXU, 1);
+
+	if (openned_semaphore == SEM_FAILED){
+		perror("sem_open");
+		return -1;
+		}
+	else {
+		printf("Semaphore [%s] openned successfully\n", sem_name);
+		*semaphore = openned_semaphore;
+		return 0;
+	}
+}
+
